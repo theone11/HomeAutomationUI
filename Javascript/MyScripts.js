aab6da31a759540b87c75cac8c61f9eb366074d8
theone111 <kessler.dan1@gmail.com>
2016-07-29 00:02:59 +0300

    2016.07.29
    *Incorporate UpdateDomoticzData back into CheckHardwareStatus and move
    Domoticz server back into Hardware
    *Move Domoticz Server from "server" back to "hardware", Remove
    DataUpdateInterval parameter
    *Find Log URL by looking for Domoticz Server (FOR) in hardware (break
    when found)
    *Change references of "Domoticz Server" in code to parameters - same as
    was done for "Broadlink RM Bridge"
    *Rename CheckUpdateStatusInterval to CheckHardwareStatusInterval
    * Added weather information and analog clock

--- Javascript/MyScripts.js
+++ Javascript/MyScripts.js
@@ -1,11 +1,16 @@
 function onloadIntervals() {
-  UpdateDomoticzData();
-  UpdateDomoticzLog();
+  CreateHTML();
   CheckHardwareStatus();
-  setInterval(UpdateDomoticzLog, myXMLconfig.js_parameters.LogUpdateInterval);
-  setInterval(UpdateDomoticzData, myXMLconfig.js_parameters.DataUpdateInterval);
-  setInterval(CheckHardwareStatus, myXMLconfig.js_parameters.CheckUpdateStatusInterval);
+  UpdateDomoticzLog();
+  setInterval(UpdateDomoticzLog, myXMLconfig.js_parameters.UpdateDomoticzLogInterval);
+  setInterval(CheckHardwareStatus, myXMLconfig.js_parameters.CheckHardwareStatusInterval);
 }
+
+
+function CreateHTML () {
+  
+}
+
 
 function openTab(evt, tabName) {
  
@@ -29,70 +34,49 @@ function openTab(evt, tabName) {
 }
 
 
-function UpdateDomoticzData(){
-
-  console.log(arguments.callee.name + ' - ' + Math.round(new Date().getTime()) + ' [ms]');
-  
-  var DomoticzDataURL = 'http://' + myXMLconfig.server.domoticz.ip + ':' + myXMLconfig.server.domoticz.port + myXMLconfig.server.domoticz.dataURL;
-  //console.log(DomoticzDataURL);
-
-//  $.getJSON(DomoticzDataURL, {format: "json"}, function(data) {DomoticzDataAnalyze(data);});
-
-   $.ajax({
-          url: DomoticzDataURL,
-          dataType: "json",
-          async: true,
-          timeout: ((myXMLconfig.server.domoticz.timeout == "") ? myXMLconfig.js_parameters.XMLHttpRequestTimeout : myXMLconfig.server.domoticz.timeout),
-          success: function(data) {DomoticzDataAnalyze(data);},
-          error: function () {}
-        });
-
-  function DomoticzDataAnalyze(json) {
-    //console.log(json);
-    if (json.status == "OK") {
-      document.getElementById("sunrise").innerHTML = "Sunrise " + json.Sunrise;
-      document.getElementById("sunset").innerHTML = "Sunset " + json.Sunset;
-    } else {
-      document.getElementById("sunrise").innerHTML = "Sunrise error";
-      document.getElementById("sunset").innerHTML = "Sunset error";
-    }
-    //console.log(arguments.callee.name + ' - ' + Math.round(new Date().getTime()) + ' [ms]');
-  }
-}
-
-
 function UpdateDomoticzLog() {
 
   console.log(arguments.callee.name + ' - ' + Math.round(new Date().getTime()) + ' [ms]');
   
-  var DomoticzLogURL = 'http://' + myXMLconfig.server.domoticz.ip + ':' + myXMLconfig.server.domoticz.port + myXMLconfig.server.domoticz.logURL;
-  //console.log(DomoticzLogURL);
+  var index;
+  var DomoticzLogURL = "";
+  for (i = 0; i < myXMLconfig.hardware.component.length; i++) {
+    if (myXMLconfig.hardware.component[i].type == "Domoticz Server") {
+      DomoticzLogURL = 'http://' + myXMLconfig.hardware.component[i].ip + ':' + myXMLconfig.hardware.component[i].port + myXMLconfig.hardware.component[i].logURL;
+      //console.log(DomoticzLogURL);
+      index = i;
+      break;
+    }
+  }
 
-//  $.getJSON(DomoticzLogURL, {format: "json"}, function(data) {DomoticzLogAnalyze(data);});
-
+  //$.getJSON(DomoticzLogURL, {format: "json"}, function(data) {DomoticzLogAnalyze(data);});
   $.ajax({
           url: DomoticzLogURL,
           dataType: "json",
           async: true,
-          timeout: ((myXMLconfig.server.domoticz.timeout == "") ? myXMLconfig.js_parameters.XMLHttpRequestTimeout : myXMLconfig.server.domoticz.timeout),
-          success: function(data) {DomoticzLogAnalyze(data);},
+          timeout: ((myXMLconfig.hardware.component[index].timeout == "") ? myXMLconfig.js_parameters.XMLHttpRequestTimeout : myXMLconfig.hardware.component[index].timeout),
+          indexValue: index,
+          success: function(data) {DomoticzLogAnalyze(data, this.indexValue);},
           error: function () {
                    document.getElementById("Domoticz Server" + "_Status").style.color = "red";
                    document.getElementById("Log Content").innerHTML = "Server could not be reached";
                  }
         });
 
-  function DomoticzLogAnalyze(json) {
+  function DomoticzLogAnalyze(json, index) {
+
+    //console.log(arguments.callee.name + ' - ' + Math.round(new Date().getTime()) + ' [ms]');
+
     //console.log(json);
     if (json.status == "OK") {
-      document.getElementById("Domoticz Server" + "_Status").style.color = "lightgreen";
+      document.getElementById(myXMLconfig.hardware.component[index].type + "_Status").style.color = "lightgreen";
       var logtext = "";
       for (i = (json.result.length - 1); i >= 0; i--) {
         logtext += json.result[i].message + "<br>";
       }
       document.getElementById("Log Content").innerHTML = logtext;
     } else {
-      document.getElementById("Domoticz Server" + "_Status").style.color = "red";
+      document.getElementById(myXMLconfig.hardware.component[index].type + "_Status").style.color = "red";
       document.getElementById("Log Content").innerHTML = "Server returned Status = " + json.status;
     }
     console.log(arguments.callee.name + ' - ' + Math.round(new Date().getTime()) + ' [ms]');
@@ -108,6 +92,18 @@ function CheckHardwareStatus() {
   for (var i = 0; i < myXMLconfig.hardware.component.length; i++) {
     //console.log(myXMLconfig.hardware.component[i].type);
     switch (myXMLconfig.hardware.component[i].type) {
+      case "Domoticz Server" :
+        //console.log('http://' + myXMLconfig.hardware.component[i].ip + ':' + myXMLconfig.hardware.component[i].port + myXMLconfig.hardware.component[i].dataURL);
+        //  $.getJSON(DomoticzDataURL, {format: "json"}, function(data) {DomoticzDataAnalyze(data);});
+        $.ajax({
+          url: 'http://' + myXMLconfig.hardware.component[i].ip + ':' + myXMLconfig.hardware.component[i].port + myXMLconfig.hardware.component[i].dataURL,
+          dataType: "json",
+          async: true,
+          timeout: ((myXMLconfig.hardware.component[i].timeout == "") ? myXMLconfig.js_parameters.XMLHttpRequestTimeout : myXMLconfig.hardware.component[i].timeout),
+          indexValue: i,
+          success: function(data) {DomoticzDataAnalyze(data, this.indexValue);},
+          error: function () { document.getElementById(myXMLconfig.hardware.component[i].type + "_Status").style.color = "red";  }
+        });
       case "MKTronic LAN Relay Board" :
         //console.log('http://' + myXMLconfig.hardware.component[i].ip + ':' + myXMLconfig.hardware.component[i].port + ' ' + Math.round(new Date().getTime()) + ' [ms]');
         break;
@@ -147,7 +143,21 @@ function CheckHardwareStatus() {
   }
 }
 
-function BroadlinkProbeDevices(json, i){
+function DomoticzDataAnalyze(json, i) {
+
+  //console.log(arguments.callee.name + ' - ' + Math.round(new Date().getTime()) + ' [ms]');
+
+  //console.log(json);
+  if (json.status == "OK") {
+    document.getElementById("sunrise").innerHTML = "Sunrise " + json.Sunrise;
+    document.getElementById("sunset").innerHTML = "Sunset " + json.Sunset;
+  } else {
+    document.getElementById("sunrise").innerHTML = "Sunrise error";
+    document.getElementById("sunset").innerHTML = "Sunset error";
+  }
+}
+
+function BroadlinkProbeDevices(json, i) {
 
   //console.log(arguments.callee.name + ' - ' + Math.round(new Date().getTime()) + ' [ms]');
 
@@ -171,7 +181,7 @@ function BroadlinkProbeDevices(json, i){
   }
 }
 
-function BroadlinkRegisteredDevices(json, i){
+function BroadlinkRegisteredDevices(json, i) {
 
   //console.log(arguments.callee.name + ' - ' + Math.round(new Date().getTime()) + ' [ms]');
 
